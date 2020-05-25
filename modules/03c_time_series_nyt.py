9313c9cd9f2946f8c6adf1bc370ba8688c9c3926
wyu <kharma@gmail.com>
2020-05-25 12:20:07 -0700

    organize plot modules

--- modules/03c_time_series_nyt.py
+++
@@ -1,8 +0,0 @@
-# 03c - create time series
-
-# create chart and adjust figure size
-# ref: https://www.drawingfromdata.com/setting-figure-size-using-seaborn-and-matplotlib
-df_california.plot(x='date', y='cases', figsize=(20,10))
-plt.title(
-    'Time Series: Total CA Cases by County (As of 04-03-2020)'
-)
