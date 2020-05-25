e972a72d1c3d473ccf91ed0c6733324ff387ccba
wyu <kharma@gmail.com>
2020-05-25 10:27:43 -0700

    organize module func

--- modules/02a.01_import_csv.py
+++ modules/02a.01_import_csv.py
@@ -1,43 +1,30 @@
-# 02A.01 - Load data into notebook
+# 02A.01 - load data into notebook
 
 # load data via file
 # Source data: https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
-# df_total = pd.read_csv('data/nyt_county_cases.csv')
+df_total = pd.read_csv('../data/Ridership_by_Route_Cumulative_Monthly.csv')
 
-# nytimes dataset: load data via url
+# load data via url
 # https://stackoverflow.com/questions/32400867/pandas-read-csv-from-url/41880513#41880513
-url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-df_total = pd.read_csv(url)
+# url = ""
+# df_total = pd.read_csv(url)
 
-# nytimes dataset: filter by state
+# filter by line type
 # https://stackoverflow.com/questions/11869910/pandas-filter-rows-of-dataframe-with-operator-chaining
-df_california = df_total[df_total['state'] == 'California']
-
-# kff dataset: load data via file
-# Source data: https://www.kff.org/health-costs/issue-brief/state-data-and-policy-actions-to-address-coronavirus/#stateleveldata
-df_total_kff = pd.read_csv('data/kff_state_testing_v1.csv')
+df_core = df_total[df_total['LineType'] == 'Core']
 
 # output table info
 def data_profile(df, msg):
     print(msg, '\n')
-    print('dataframe shape: ', df.shape, '\n')
+    print('Dataframe Shape:', df.shape, '\n')
     print(df.info(), '\n')
-    print('dataframe data types:\n\n', df.dtypes, '\n')
+    print('Dataframe Data Types:\n')
+    print(df.dtypes, '\n')
 
-# nytimes dataset
+# data profile
 data_profile(
     df_total,
-    '*** Table Info: NYT Dataset - Total Cases by US County ***'
-)
-# nytimes dataset
-data_profile(
-    df_california,
-    '*** Table Info: NYT Dataset - Total Cases in CA ***'
-)
-# kff dataset
-data_profile(
-    df_total_kff,
-    '*** Table Info: KFF Dataset - Total Results by US State ***'
+    '*** Table Info: VTA Ridership - Cumulative Monthly Total from 2005-2018 ***'
 )
 
 # output unique values
@@ -45,15 +32,13 @@ def show_unique(col, msg):
     print('\n', msg, '\n')
     print(col.unique())
 
-# nytimes dataset
 show_unique(
-    df_total['state'],
-    '*** Unique Values: NYT Dataset - US States ***'
+    df_total['Routes'],
+    '*** Unique Values: VTA Ridership - Train Route ***'
 )
-# kff dataset
 show_unique(
-    df_total_kff['location'],
-    '*** Unique Values: KFF Dataset - US States ***'
+    df_total['LineType'],
+    '*** Unique Values: VTA Ridership - Train Line Type ***'
 )
 
 # output summary stats
@@ -61,18 +46,11 @@ def summary_stats(col, msg):
     print('\n', msg, '\n')
     print(col.describe())
 
-# nytimes dataset
 summary_stats(
-    df_total['cases'],
-    '*** Summary Stats: NYT Dataset - Total Cases by US County ***'
+    df_total['Boardings'],
+    '*** Summary Stats: VTA Ridership - Train Boardings for All Routes ***'
 )
-# kff dataset
 summary_stats(
-    df_total_kff['tests_with_results'],
-    '*** Summary Stats: KFF Dataset - Total Tests with Results by State ***'
-)
-# kff dataset
-summary_stats(
-    df_total_kff['total_tests_per_1k_residents'],
-    '*** Summary Stats: KFF Dataset - Total Tests per 1k Residents by State ***'
+    df_core['Boardings'],
+    '*** Summary Stats: VTA Ridership - Train Boardings for Core Lines ***'
 )
