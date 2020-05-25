8a6ba0597c7e48fa4f88940c97f3181ad838989d
wyu <kharma@gmail.com>
2020-05-25 12:20:45 -0700

    organize plot modules

---
+++ modules/03b.02_time_series.py
@@ -0,0 +1,21 @@
+# 03B.02 - create time series
+
+# set plot size and title
+plt.figure(figsize=(16, 12))
+
+# create time series plot
+def time_series(x_col, y_col, df, msg):
+    sns.lineplot(
+        x=x_col,
+        y=y_col,
+        data=df
+    )
+    plt.title(msg)
+    plt.show()
+
+time_series(
+    'Period',
+    'Boardings',
+    df_total,
+    'VTA Ridership - Cumulative Monthly Total (2005-2018): Train Boardings by Line Type'
+)
